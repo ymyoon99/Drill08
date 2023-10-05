44ac535032250dddd262fccaf512828e593d3a64
ymyoon(OMEN) <ozone753@naver.com>
2023-10-05 10:28:24 +0900

    기본 틀 작성

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,5 +1,6 @@
 from pico2d import *
 
+
 # Game object class here
 
 def handle_events():
@@ -11,11 +12,36 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
+
+open_canvas()
+
+
+def reset_world():
+    global running
+
+    running = True
+
+
+def update_world():
+    pass
+
+
+def render_world():
+    clear_canvas()
+    update_canvas()
+
+
 open_canvas()
 
 # initialization code
+reset_world()
 
 # game main loop code
+while running:
+    handle_events() # game logic
+    update_world() # game logic
+    render_world() # draw world
+    delay(0.05)
 
 # finalization code
 
