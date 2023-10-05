00610262027611b444be6c330b94674260c13f0e
ymyoon(OMEN) <ozone753@naver.com>
2023-10-05 11:24:36 +0900

    구현실패

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -3,6 +3,8 @@ import random
 
 
 # Game object class here
+
+
 class Grass: # class의 이름은 명사로, 첫 글자는 대문자로.
     def __init__(self): # 생성자 함수, 객체가 생성이 될때 항상 맨처음 불러와 짐. 객체의 초기상태 설정.
         self.image = load_image('grass.png') # self는 생성된 객체를 가르키는 변수, self 안에 속성이 담김.
@@ -27,6 +29,18 @@ class Boy:
         self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
 
 
+class Ball:
+    def __init__(self):
+        self.x, self.y = random.randint(100, 700), 599
+        self.image = load_image('ball21x21.png')
+
+    def update(self):
+        self.y -= random.randint(1,10)
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+
 def handle_events():
     global running
     events = get_events()
@@ -44,17 +58,22 @@ def reset_world():
     global running
     global grass
     global team
+    global falling_ball
     global world
 
     running = True
     world = [] # 새로운 객체를 추가할 때 update_world와 render_world의 코드를 수정하지 않아도 됨.
-    # 새로운 객체를 생성하면 world list에만 넣어주면 됨.
+    # 새로운 객체를 생성하면 world list에 만 넣어주면 됨.
 
     grass = Grass() # 파라미터가 들어갈 수 있기 때문에 ()를 해주어야함.
     world.append(grass)
 
     team = [Boy() for i in range(10)] # List comperhension 이용
     world += team
+
+    falling_ball = [Ball() for n in range(10)]
+    world += falling_ball
+
 
 
 def update_world():
