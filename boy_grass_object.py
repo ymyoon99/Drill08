e1daa818d3b7ae4b754ae613babb288719822aa1
ymyoon(OMEN) <ozone753@naver.com>
2023-10-09 13:30:21 +0900

    Drill8 기능구현

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -3,16 +3,14 @@ import random
 
 
 # Game object class here
+class Grass:  # class의 이름은 명사로, 첫 글자는 대문자로.
+    def __init__(self):  # 생성자 함수, 객체가 생성이 될때 항상 맨처음 불러와 짐. 객체의 초기상태 설정.
+        self.image = load_image('grass.png')  # self는 생성된 객체를 가르키는 변수, self 안에 속성이 담김.
 
+    def draw(self):  # 잔디를 그려주는 행위를 함.
+        self.image.draw(400, 30)  # self로 가르켜야함.
 
-class Grass: # class의 이름은 명사로, 첫 글자는 대문자로.
-    def __init__(self): # 생성자 함수, 객체가 생성이 될때 항상 맨처음 불러와 짐. 객체의 초기상태 설정.
-        self.image = load_image('grass.png') # self는 생성된 객체를 가르키는 변수, self 안에 속성이 담김.
-
-    def draw(self): # 잔디를 그려주는 행위를 함.
-        self.image.draw(400, 30) # self로 가르켜야함.
-
-    def update(self): pass # 통일을 위한 dummy 함수이다.
+    def update(self): pass  # 통일을 위한 dummy 함수이다.
 
 
 class Boy:
@@ -22,11 +20,11 @@ class Boy:
         self.image = load_image('run_animation.png')
 
     def update(self):
-        self.frame = random.randint(0,7)
-        self.x += 5 # 오른쪽으로 이동
+        self.frame = random.randint(0, 7)
+        self.x += 5  # 오른쪽으로 이동
 
     def draw(self):
-        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
 
 
 class Ball:
@@ -35,7 +33,25 @@ class Ball:
         self.image = load_image('ball21x21.png')
 
     def update(self):
-        self.y -= random.randint(1,10)
+        if self.y > 70:
+            self.y -= random.randint(1, 10)
+        else:
+            self.y = 70
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+
+class Ball2:
+    def __init__(self):
+        self.x, self.y = random.randint(100, 700), 599
+        self.image = load_image('ball41x41.png')
+
+    def update(self):
+        if self.y > 75:
+            self.y -= random.randint(1, 10)
+        else:
+            self.y = 75
 
     def draw(self):
         self.image.draw(self.x, self.y)
@@ -59,35 +75,46 @@ def reset_world():
     global grass
     global team
     global falling_ball
+    global falling_ball2
     global world
 
     running = True
-    world = [] # 새로운 객체를 추가할 때 update_world와 render_world의 코드를 수정하지 않아도 됨.
+    world = []  # 새로운 객체를 추가할 때 update_world와 render_world의 코드를 수정하지 않아도 됨.
     # 새로운 객체를 생성하면 world list에 만 넣어주면 됨.
 
-    grass = Grass() # 파라미터가 들어갈 수 있기 때문에 ()를 해주어야함.
+    grass = Grass()  # 파라미터가 들어갈 수 있기 때문에 ()를 해주어야함.
     world.append(grass)
 
-    team = [Boy() for i in range(10)] # List comperhension 이용
+    team = [Boy() for i in range(10)]  # List comperhension 이용
     world += team
 
     falling_ball = [Ball() for n in range(10)]
     world += falling_ball
 
+    falling_ball2 = [Ball2() for k in range(10)]
+    world += falling_ball2
 
 
 def update_world():
     grass.update()
     for o in team:
         o.update()
+    for n in falling_ball:
+        n.update()
+    for i in falling_ball2:
+        i.update()
 
 
 def render_world():
     clear_canvas()
-    grass.draw() # clear와 update 사이에 glass를 그려줌.
+    grass.draw()  # clear와 update 사이에 glass를 그려줌.
     # 여러개의 오브젝트를 그릴 때는 그리는 순서가 중요함.
     for o in team:
         o.draw()
+    for n in falling_ball:
+        n.draw()
+    for i in falling_ball2:
+        i.draw()
     update_canvas()
 
 
@@ -98,9 +125,9 @@ reset_world()
 
 # game main loop code
 while running:
-    handle_events() # game logic
-    update_world() # game logic
-    render_world() # draw world
+    handle_events()  # game logic
+    update_world()  # game logic
+    render_world()  # draw world
     delay(0.05)
 
 # finalization code
